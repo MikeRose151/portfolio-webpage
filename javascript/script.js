f48acb2628c51fd2bfb80c5062b3ce52fd208eb8
Mike Rose <michaelrose151@gmail.com>
2023-10-20 11:33:14 -0400

    got the modal working for the about me section

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,34 @@
 "use strict";
 
+// Get the modal
+const modal = document.getElementById("my-modal");
+
+// Get the button that opens the modal
+const modalLinks = Array.from(document.getElementsByClassName("modal-link"));
+console.log(modalLinks);
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the modal links, open the modal
+modalLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
 // Define an array of project card IDs and corresponding image IDs
 const projectCardIds = ["tat", "enna", "ten-number-challenge", "rar"];
 
